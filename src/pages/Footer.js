deecf57a70d75d82f4eb67a736dd08466a7bbcf5
asyraf <asyraf@cybersolution.com.my>
2025-04-12 06:10:44 +0800

    -fixed footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,22 +1,33 @@
-  import React from 'react';
-  import styles from './components/css/Footer.module.css';
-  import footerData from './components/data/footer.json';
-  import SocialIconList from './components/reuseComponent/SocialIconList';
-  import FooterLinkSection from './components/reuseComponent/FooterLinkSection';
+import React from "react";
+import styles from "./components/css/Footer.module.css";
+import footerData from "./components/data/footer.json";
+import SocialIconList from "./components/reuseComponent/SocialIconList";
+import FooterLinkSection from "./components/reuseComponent/FooterLinkSection";
 
-  export default function Footer() {
-    return (
-      <footer className={styles.footer}>
-        <div className={styles.footerContainer}>
-          <div>
-            <h7>Software Recruitment co.</h7>
-            <SocialIconList icons={footerData.socialIcons|| []} />
-          </div>
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContainer}>
+      <div className={styles.footerTop}>
+      <h7>Software Recruitment co.</h7>
+      <SocialIconList icons={footerData.socialIcons || []} />
+    </div>
 
-          <FooterLinkSection title="Explore" links={footerData.exploreLinks|| []} />
-          <FooterLinkSection title="Sectors" links={footerData.sectorLinks|| []} />
-          <FooterLinkSection title="Services" links={footerData.serviceLinks|| []} />
+        <div className={styles.footerLinksWrapper}>
+          <FooterLinkSection
+            title="Explore"
+            links={footerData.exploreLinks || []}
+          />
+          <FooterLinkSection
+            title="Sectors"
+            links={footerData.sectorLinks || []}
+          />
+          <FooterLinkSection
+            title="Services"
+            links={footerData.serviceLinks || []}
+          />
         </div>
-      </footer>
-    );
-  }
+      </div>
+    </footer>
+  );
+}
